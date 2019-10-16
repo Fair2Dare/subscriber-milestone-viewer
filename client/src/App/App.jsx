56479f06ecafb5fc35052fda3b548fa2c4a5c14c
Fair2Dare <vinmags16@gmail.com>
2019-10-15 22:05:04 -0500

    Sucessful Twitch login

---
+++ client/src/App/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './App.css';
+import { Button } from 'react-bootstrap';
+import SearchPage from '../SearchPage/SearchPage';
+
+export default class App extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userId: '',
+      access_token: '',
+      content: ''
+    };
+  }
+
+  // handleClick = () => {
+  //   fetch('/auth/twitch').then(response => console.log(response));
+  // }
+
+  render() {
+    const loginPage = (
+      <div className="LoginPage">
+        <Button size="lg" type="primary" href="/auth/twitch">
+          Login to Twitch
+        </Button>
+        <br/>
+      </div>
+    );
+    return this.state.userId === '' ? (
+      loginPage
+    ) : (
+      <SearchPage userId={this.state.userId} />
+    );
+  }
+}
